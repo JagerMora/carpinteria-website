507aded1430646d9805d59b3f7f2732e302c5420
JagerMora <78835594+MissaelMoraCar-E@users.noreply.github.com>
2023-11-19 22:44:55 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// @ts-expect-error ignore
+import Header from './components/header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import './header.css'
+
+export const Header = () => {
+    /**
+     * goes here:
+     * declarations, hooks, handlers, const
+     */
+
+    return (
+        <div className="headerContainer">
+            <div>
+                <h1>Left</h1>
+            </div>
+            <div>
+                <h1>Right</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
